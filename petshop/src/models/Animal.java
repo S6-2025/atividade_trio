a013a6e9d6c8b30a0a142c7898b2979d15cdbd10
gabriellesote <gabrielle.sote@gmail.com>
2025-03-13 07:30:29 -0300

    Desenvolvimento do Animal.java

--- petshop/src/models/Animal.java
+++ petshop/src/models/Animal.java
@@ -1,3 +1,74 @@
+package models;
+
 public class Animal {
-  
+  private String especie;
+  private String raca;
+  private String cor;
+  private String nome;
+  private String dono;
+  private int idade;
+
+  public Animal(String especie, String raca, String cor, int idade) {
+    this.especie = especie;
+    this.raca = raca;
+    this.cor = cor;
+    this.idade = idade;
+  }
+
+  public void setEspecie(String especie) {
+    this.especie = especie;
+  }
+  public String getEspecie() {
+    return especie;
+  }
+
+  public void setRaca(String raca) {
+    this.raca = raca;
+  }
+  public String getRaca() {
+    return raca;
+  }
+
+  public void setCor(String cor) {
+    this.cor = cor;
+  }
+  public String getCor() {
+    return cor;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+  public int getIdade() {
+    return idade;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getNome() {
+    return nome;
+  }
+
+  public void setDono(String dono) {
+    this.dono = dono;
+  }
+  public String getDono() {
+    return dono;
+  }
+
+
+  public String Andar() {
+    return nome;
+  }
+
+  public String Correr() {
+    return nome;
+  }
+
+  @Override
+  public String toString() {
+    return "Espécie:" + especie + "Raça: " + raca + "Cor: " + cor + "Idade:" + idade;
+  }
+
 }
